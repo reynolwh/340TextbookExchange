c6e8565ee7773e904c2466ad67e410889d716d3e
Morgan Benton <registered@morphatic.com>
2012-10-18 15:05:52 -0400

    updated DB structure

--- textbooks.sql
+++ textbooks.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2012 at 08:29 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: Oct 18, 2012 at 03:04 PM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -31,40 +32,25 @@ CREATE TABLE IF NOT EXISTS `textbooks` (
   `ISBN` varchar(20) NOT NULL,
   `Condition` enum('Like New','Good','Acceptable','Poor') NOT NULL DEFAULT 'Acceptable',
   `Price` varchar(8) NOT NULL,
-  `User ID` int(10) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `User ID` (`User ID`)
+  `userID` int(10) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `textbooks`
---
-
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user id`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `user id` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `Email` varchar(100) NOT NULL,
-  `Password` varchar(20) NOT NULL,
-  UNIQUE KEY `ID` (`ID`,`Email`,`Password`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `user id`
---
-
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `textbooks`
---
-ALTER TABLE `textbooks`
-  ADD CONSTRAINT `textbooks_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `user id` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
